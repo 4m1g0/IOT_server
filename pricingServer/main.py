711e30d25c443cba2c705a73c0540cb71a5a46bd
4m1g0 <dev.4m1g0@gmail.com>
2016-08-17 01:03:12 +0200

    Catch exceptions and return 404 if no info returned

--- pricingServer/main.py
+++ pricingServer/main.py
@@ -4,15 +4,20 @@ import requests
 import time
 import json
 
-hostName = "localhost"
+hostName = ""
 hostPort = 9000
 
 def getJson(url):
     r = requests.get(url)
-    doc = xmltodict.parse(r.text)
+    doc = {}
     lista = []
-    for i in doc['PVPCDesgloseHorario']['SeriesTemporales'][7]['Periodo']['Intervalo']:
-        lista.append(int(float(i['Ctd']['@v']) * 1000000))
+    try:
+        doc = xmltodict.parse(r.text)
+    
+        for i in doc['PVPCDesgloseHorario']['SeriesTemporales'][7]['Periodo']['Intervalo']:
+            lista.append(int(float(i['Ctd']['@v']) * 1000000))
+    except:
+        pass
     return lista
     
 
@@ -22,10 +27,13 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         lista = getJson("https://api.esios.ree.es/archives/80/download?date=" + self.path[1:])
         
-        self.send_response(200)
-        self.send_header("Content-type", "application/javascript")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(lista), 'utf-8'))
+        if lista == []:
+            self.send_response(404)
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "application/javascript")
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(lista), 'utf-8'))
 
 myServer = HTTPServer((hostName, hostPort), MyServer)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
