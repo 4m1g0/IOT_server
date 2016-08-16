51e0e65e22cdbacd9b4ce29414cf661ab5ae14c5
4m1g0 <dev.4m1g0@gmail.com>
2016-08-16 21:11:40 +0200

    Mini http server to prepare ree data in json

---
+++ pricingServer/main.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import xmltodict
+import requests
+import time
+import json
+
+hostName = "localhost"
+hostPort = 9000
+
+def getJson(url):
+    r = requests.get(url)
+    doc = xmltodict.parse(r.text)
+    lista = []
+    for i in doc['PVPCDesgloseHorario']['SeriesTemporales'][7]['Periodo']['Intervalo']:
+        lista.append(int(float(i['Ctd']['@v']) * 1000000))
+    return lista
+    
+
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        lista = getJson("https://api.esios.ree.es/archives/80/download?date=" + self.path[1:])
+        
+        self.send_response(200)
+        self.send_header("Content-type", "application/javascript")
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(lista), 'utf-8'))
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
